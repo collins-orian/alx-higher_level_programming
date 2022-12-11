49aee2e4c81f97320b5c328e47565d02bfadba74
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2022-12-11 17:21:05 +0100

    Create 12-roman_to_int.py
    some work still needed to be done

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if isinstance(roman_string, str):
+        for i in roman_string:
+            if i in roman.keys():
+				
+    else:
+        return None
