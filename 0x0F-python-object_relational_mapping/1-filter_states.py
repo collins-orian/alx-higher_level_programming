e7e9e2f5763d430135d48c428f4931bff7632de2
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-03-23 23:41:23 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         password=argv[2], database=argv[3])
     cursor = con.cursor()
     cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
     db = cursor.fetchall()
     for i in db:
         print(i)
