252905ddf0a58e5fb4a83f7f10c60c1b2026943f
Oshioregbeme Collins Inaboya <111002610+collins-orian@users.noreply.github.com>
2023-04-11 06:00:29 +0100

    corrections made

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-# Script that lists all states from the database
+"""
+Displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+(Safe from SQL Injection)
+"""
 
 
 import MySQLdb
@@ -8,14 +12,16 @@ from sys import argv
 
 
 if __name__ == "__main__":
+
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
     cursor = con.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name='{:s}' ORDER BY id ASC;".format(argv[4]))
+        "SELECT * FROM states WHERE name ='{:s}' ORDER BY id ASC;".format(argv[4]))
     db = cursor.fetchall()
     for i in db:
         print(i)
+
     cursor.close()
     con.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,15 +4,24 @@
 
 import MySQLdb
 from sys import argv
-
+import re
 
 if __name__ == "__main__":
+    if (len(argv) != 5):
+        print('Use: username, password, database name, state name')
+        exit(1)
+
+    searched = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z ]+$', searched) is None):
+        print('Enter a valid name state (example: Arizona)')
+        exit(1)
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
     cursor = con.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC;".format(argv[4]))
+        "SELECT * FROM states WHERE name = {:s} ORDER BY id ASC;".format(argv[4]))
     db = cursor.fetchall()
     for i in db:
         print(i)
